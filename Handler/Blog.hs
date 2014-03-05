0ff2ad5f95e894f9811cc94065824c166125541d
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2014-03-05 14:55:23 +0900

    Blog GET

---
+++ Handler/Blog.hs
@@ -0,0 +1,26 @@
+module Handler.Blog
+    ( getBlogR
+    , postBlogR
+    )
+where
+
+import Import
+
+import Yesod.Form.Nic (YesodNic, nicHtmlField)
+instance YesodNic App
+
+getBlogR :: Handler Html
+getBlogR = do
+    articles <- runDB $ selectList [] [Desc ArticleTitle]
+    (articleWidget, enctype) <- generateFormPost entryForm
+    defaultLayout $ do
+        $(widgetFile "articles")
+
+postBlogR :: Handler Html
+postBlogR = error "Not yet implemented: postBlogR"
+
+entryForm :: Form Article
+entryForm =
+    renderDivs $ Article
+    <$> areq textField "Title" Nothing
+    <*> areq nicHtmlField "Content" Nothing
